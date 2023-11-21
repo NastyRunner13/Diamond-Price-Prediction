552d9a1228bc900df16365236223a515a8f87efd
Prince Gupta <princegupta1586@gmail.com>
2023-11-21 17:13:01 +0530

    Prediction Pipeline Complete

--- src/utils.py
+++ src/utils.py
@@ -52,3 +52,12 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info("Exception Occured in load_obj function utils")
+        raise CustomException(e, sys)
